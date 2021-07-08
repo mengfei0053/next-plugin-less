98d465e89096bc67377628db0c5b009a46cbda38
孟鸿飞 <mengfei0053@live.com>
2021-07-08 02:23:58 +0000

    ci(release.config.js): update releaseRules

--- release.config.js
+++ release.config.js
@@ -101,11 +101,8 @@ module.exports = {
           { type: 'feat', release: 'minor' },
           { type: 'fix', release: 'patch' },
           { type: 'perf', release: 'patch' },
-          { type: 'docs', release: 'patch' },
           { type: 'refactor', release: 'patch' },
           { type: 'style', release: 'patch' },
-          { type: 'chore', release: 'patch' },
-          { type: 'ci', release: 'patch' }
         ]
       }
     ],
