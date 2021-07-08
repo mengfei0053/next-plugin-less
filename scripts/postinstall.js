01e86a5f9659b7032037b0c6886da6cf7eaa4067
孟鸿飞 <mengfei0053@live.com>
2021-07-08 04:01:56 +0000

    ci(project): fix github auto push

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,11 +1,14 @@
+
 const simpleGit = require('simple-git');
 const git = simpleGit();
 
+const { GITHUB_NAME, GITHUB_AUTH } = process.env;
 (async () => {
   const status = await git.status();
   const currentBranch = status.current;
-  await git.push('origin', `${currentBranch}:master`);
+  const origin = `https://${GITHUB_NAME}:${GITHUB_AUTH}@github.com/mengfei0053/next-plugin-less.git`;
+  await git.push(origin, `${currentBranch}:master`);
   if (currentBranch === 'release') {
-    await git.push('origin', `${currentBranch}:alpha`);
+    await git.push(origin, `${currentBranch}:alpha`);
   }
 })();
